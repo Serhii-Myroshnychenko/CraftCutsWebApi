33f85cb7184b4091bbc0a24379f6e77fce80d4d6
Serhii-Myroshnychenko <serhii.myroshnychenko@nure.ua>
2021-10-15 20:07:27 +0300

    create Blog logic

---
+++ CraftCutsTestApiProject/Contracts/IBlogRepository.cs
@@ -0,0 +1,17 @@
+﻿using CraftCutsTestApiProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftCutsTestApiProject.Contracts
+{
+    public interface IBlogRepository
+    {
+        public Task<IEnumerable<Blog>> GetBlogs();
+        public Task<Blog> GetBlog(int id);
+        public Task CreateBlog(Blog blog);
+        public Task UpdateBlog(int id, Blog blog);
+        public Task DeleteBlog(int id);
+    }
+}
---
+++ CraftCutsTestApiProject/Repositories/BlogRepository.cs
@@ -0,0 +1,55 @@
+﻿using CraftCutsTestApiProject.Context;
+using CraftCutsTestApiProject.Contracts;
+using CraftCutsTestApiProject.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftCutsTestApiProject.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly DapperContext _context;
+        public BlogRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBlog(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Blog> GetBlog(int id)
+        {
+            var query = "SELECT * FROM Blog Where blog_id = @id";
+            using(var connection = _context.CreateConnection())
+            {
+                var blog = await connection.QuerySingleOrDefaultAsync<Blog>(query, new { id });
+                return blog;
+            }
+        }
+
+        public async Task<IEnumerable<Blog>> GetBlogs()
+        {
+            var query = "SELECT * FROM Blog";
+            using (var connection = _context.CreateConnection())
+            {
+                var blogs = await connection.QueryAsync<Blog>(query);
+                return blogs.ToList();
+            }
+        }
+
+        public Task UpdateBlog(int id, Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
