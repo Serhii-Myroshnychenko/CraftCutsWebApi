ee69d6af029ff44583e20d46ef13e1b7d2d09bab
Serhii-Myroshnychenko <serhii.myroshnychenko@nure.ua>
2021-10-15 20:13:30 +0300

    create BlogController

---
+++ CraftCutsTestApiProject/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using CraftCutsTestApiProject.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftCutsTestApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogRepository _blogRepository;
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs()
+        {
+            try
+            {
+                var blogs = await _blogRepository.GetBlogs();
+                return Ok(blogs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
