6a41560353ff5703d43ad01e3a1f5e2f792e4dd6
Serhii-Myroshnychenko <serhii.myroshnychenko@nure.ua>
2021-11-18 17:28:15 +0200

    update validation

--- CraftCutsTestApiProject/Models/Registration.cs
+++ CraftCutsTestApiProject/Models/Registration.cs
@@ -9,14 +9,18 @@ namespace CraftCutsTestApiProject.Models
     public class Registration
     {
         [StringLength(70, MinimumLength = 2)]
-        [Required]
+        [Required(ErrorMessage = "Введите имя")]
+        
         public string name { get; set; }
         [StringLength(150, MinimumLength = 5)]
+        [Required(ErrorMessage = "Введите пароль")]
         public string password { get; set; }
         [RegularExpression(@"^([\w.-]+)@([\w-]+)((.(\w){2,3})+)$")]
-        [Required]
+        [Required(ErrorMessage = "Введите почту")]
+        
         public string email { get; set; }
-        [RegularExpression(@"(^+\d{1,2})?(((\d{3}))|(-?\d{3}-)|(\d{3}))((\d{3}-\d{4})|(\d{3}-\d\d-\d\d)|(\d{7})|(\d{3}-\d-\d{3}))")]
+        [Phone]
+        [Required]
         public string phone { get; set; }
         public DateTime birthday { get; set; }
 
